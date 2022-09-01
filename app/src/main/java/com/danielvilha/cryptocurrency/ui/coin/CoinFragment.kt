41aa0c9ec1f7c38bbadc64269f0332c17cd28364
Daniel Ferreira de Lima Freitas Vilha <danielvilha@gmail.com>
2022-09-01 18:30:03 +0100

    - Changing my CoinFragment.kt and HomeFragment.kt and removing my HomeViewModelFactory.kt and CoinViewModelFactory.kt.

--- app/src/main/java/com/danielvilha/cryptocurrency/ui/coin/CoinFragment.kt
+++ app/src/main/java/com/danielvilha/cryptocurrency/ui/coin/CoinFragment.kt
@@ -1,11 +1,11 @@
 package com.danielvilha.cryptocurrency.ui.coin
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.danielvilha.cryptocurrency.databinding.FragmentCoinBinding
 
 /**
@@ -17,42 +17,26 @@ class CoinFragment : Fragment() {
     private var binding: FragmentCoinBinding? = null
 
     /**
-     * Lazily initialize our [CoinViewModelFactory]
-     */
-    private val viewModelFactory: CoinViewModelFactory by lazy {
-        CoinViewModelFactory()
-    }
-
-    /**
      * Lazily initialize our [CoinViewModel]
      */
-    private val coinViewModel: CoinViewModel by lazy {
-        ViewModelProvider(this, viewModelFactory)[CoinViewModel::class.java]
-    }
+    private val coinViewModel: CoinViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        // Get a reference to the binding object and inflate the fragment views.
-        val coinBinding = FragmentCoinBinding.inflate(inflater, container, false)
-        binding = coinBinding
-
-        // I get this bundle from the last argument
-        val coinId =  arguments?.getString("id")
-
-        coinViewModel.getCoin(requireContext(), coinBinding, coinId!!)
-
-        return coinBinding.root
-    }
+    ): View = FragmentCoinBinding.inflate(inflater, container, false)
+        .apply {
+            binding = this
+            // I get this bundle from the last argument
+            val coinId =  arguments?.getString("id")
+            coinViewModel.getCoin(requireContext(), binding!!, coinId!!)
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding?.apply {
             // Specify the fragment as the lifecycle owner
             lifecycleOwner = viewLifecycleOwner
-
             viewModel = coinViewModel
         }
     }
--- app/src/main/java/com/danielvilha/cryptocurrency/ui/home/HomeFragment.kt
+++ app/src/main/java/com/danielvilha/cryptocurrency/ui/home/HomeFragment.kt
@@ -1,12 +1,12 @@
 package com.danielvilha.cryptocurrency.ui.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.danielvilha.cryptocurrency.R
 import com.danielvilha.cryptocurrency.databinding.FragmentHomeBinding
@@ -23,29 +23,17 @@ class HomeFragment : Fragment() {
     private var binding: FragmentHomeBinding? = null
 
     /**
-     * Lazily initialize our [HomeViewModelFactory]
-     */
-    private val viewModelFactory: HomeViewModelFactory by lazy {
-        HomeViewModelFactory()
-    }
-
-    /**
      * Lazily initialize our [HomeViewModel]
      */
-    private val homeViewModel: HomeViewModel by lazy {
-         ViewModelProvider(this, viewModelFactory)[HomeViewModel::class.java]
-    }
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        // Get a reference to the binding object and inflate the fragment views.
-        val homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
-        binding = homeBinding
-
-        return homeBinding.root
-    }
+    ): View = FragmentHomeBinding.inflate(inflater, container, false)
+        .apply {
+            binding = this
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
